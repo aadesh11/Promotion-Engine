6b373222a4cc7da893c270511fb28a95177773df
Aadesh Gupta <aadeshgupta1992@gmail.com>
2020-09-21 00:39:10 +0530

    Added comments for promotion service

--- src/main/java/net/engine/web/service/PromotionService.java
+++ src/main/java/net/engine/web/service/PromotionService.java
@@ -6,10 +6,25 @@ import net.engine.web.model.SkuProduct;
 import java.util.List;
 import java.util.Map;
 
+/**
+ *  All promotions types should implement it.
+ */
 public interface PromotionService {
 
+    /**
+     * To get the discounted value after applying promotions to cartItems.
+     * @param cartItemMap Map of product code vs cart item.
+     * @param productMap Map of product code vs product details.
+     * @return discounted price.
+     */
     double getDiscount(Map<String, CartItem> cartItemMap, Map<String, SkuProduct> productMap);
 
+    /**
+     * To save the promotions in database.
+     * @param items items details.
+     * @param discountPrice discount value.
+     * @return PromotionService.
+     */
     PromotionService savePromotion(List<CartItem> items, double discountPrice);
 
 }
