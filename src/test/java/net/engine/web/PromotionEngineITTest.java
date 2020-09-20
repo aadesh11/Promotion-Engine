868c874c372157b33c481d97996a63959f9d016e
Aadesh Gupta <aadeshgupta1992@gmail.com>
2020-09-21 00:44:53 +0530

    Itegration test case for Scenario A

--- src/test/java/net/engine/web/PromotionEngineITTest.java
+++ src/test/java/net/engine/web/PromotionEngineITTest.java
@@ -15,6 +15,45 @@ import static org.junit.Assert.assertTrue;
 
 public class PromotionEngineITTest {
 
+    //Scenario A as per the question
+    @Test
+    public void expectDiscountItA() {
+
+        //Add products
+        List<SkuProduct> products = new ArrayList<>();
+        products.add(new SkuProduct("A", 50));
+        products.add(new SkuProduct("B", 30));
+        products.add(new SkuProduct("C", 20));
+        products.add(new SkuProduct("D", 15));
+
+        //prepare promotions
+        PromotionService productAPromo = new ProductQuantityPromotion(new ArrayList<CartItem>() {{
+            add(new CartItem("A", 3));
+        }}, 130);
+        ProductQuantityPromotion productBPromo = new ProductQuantityPromotion(new ArrayList<CartItem>() {{
+            add(new CartItem("B", 2));
+        }}, 45);
+        ProductQuantityPromotion productCDPromo = new ProductQuantityPromotion(new ArrayList<CartItem>() {{
+            add(new CartItem("C", 1));
+            add(new CartItem("D", 1));
+        }}, 30);
+
+        List<PromotionService> promotions = new ArrayList<>();
+        promotions.add(productAPromo);
+        promotions.add(productBPromo);
+        promotions.add(productCDPromo);
+
+        //prepare cart
+        List<CartItem> carts = new ArrayList<>();
+        carts.add(new CartItem("A", 1));
+        carts.add(new CartItem("B", 1));
+        carts.add(new CartItem("C", 1));
+
+        //get the final billing amount.
+        CartService cartService = new DefaultCartService(promotions, products);
+        assertTrue("Amount should be 1000 Rs", cartService.getBillingAmount(carts) == 100.0d);
+    }
+
     //Scenario B as per the question
     @Test
     public void expectDiscountItB() {
